e6a444546cd66018800fe92b66dbbd9d591340f3
maries2345678-spec <maries2345678@gmail.com>
2026-08-18 13:48:55 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import path from 'path';
 import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
-  return {
+  return { 
+    base: '/HCMC-CultuzeHub-AI-/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
